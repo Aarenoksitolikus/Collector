18274c53345c219f97ccc0d641e676598406a310
Aarenoksitolikus <super.maxa2001@yandex.ru>
2021-12-14 17:58:00 +0300

    Add SignUpController's first version

---
+++ src/main/java/me/akvelon/collector/controllers/SignUpController.java
@@ -0,0 +1,21 @@
+package me.akvelon.collector.controllers;
+
+import me.akvelon.collector.models.User;
+import me.akvelon.collector.services.interfaces.UsersService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/signup")
+public class SignUpController {
+    @Autowired
+    private UsersService usersService;
+
+    @PostMapping
+    public ResponseEntity<User> getPage() {
+        return null;
+    }
+}
