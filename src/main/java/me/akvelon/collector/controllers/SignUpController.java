ce6b773eafad7a4fd47380f936a91f4dbea4c838
Aarenoksitolikus <super.maxa2001@yandex.ru>
2021-12-15 03:22:51 +0300

    Update SignUpController

--- src/main/java/me/akvelon/collector/controllers/SignUpController.java
+++ src/main/java/me/akvelon/collector/controllers/SignUpController.java
@@ -1,10 +1,12 @@
 package me.akvelon.collector.controllers;
 
-import me.akvelon.collector.models.User;
+import me.akvelon.collector.dto.UserForm;
+import me.akvelon.collector.services.interfaces.SignUpService;
 import me.akvelon.collector.services.interfaces.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,8 +16,11 @@ public class SignUpController {
     @Autowired
     private UsersService usersService;
 
+    @Autowired
+    private SignUpService signUpService;
+
     @PostMapping
-    public ResponseEntity<User> getPage() {
-        return null;
+    public ResponseEntity<UserForm> signUp(@RequestBody UserForm userForm) {
+        return ResponseEntity.ok(signUpService.signUp(userForm));
     }
 }
