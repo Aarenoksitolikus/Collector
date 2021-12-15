654b32beec373ecb1205665ac3b606d4baae3921
Aarenoksitolikus <super.maxa2001@yandex.ru>
2021-12-15 03:23:08 +0300

    Update UsersService

--- src/main/java/me/akvelon/collector/services/interfaces/UsersService.java
+++ src/main/java/me/akvelon/collector/services/interfaces/UsersService.java
@@ -1,11 +1,16 @@
 package me.akvelon.collector.services.interfaces;
 
+import me.akvelon.collector.dto.UserDto;
 import me.akvelon.collector.repositories.intefraces.UsersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public interface UsersService {
     void signUp();
-    void signIn();
+    List<UserDto> getAll();
+    UserDto getById(Long id);
+    UserDto getByFullName(String fullName);
 }
