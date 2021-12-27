5d53962f32e95e7a987b4cf81d0be603c940d715
Aarenoksitolikus <super.maxa2001@yandex.ru>
2021-12-27 16:19:56 +0300

    Update Page

--- src/main/java/me/akvelon/collector/models/Page.java
+++ src/main/java/me/akvelon/collector/models/Page.java
@@ -1,12 +1,14 @@
 package me.akvelon.collector.models;
 
-import lombok.Builder;
-import lombok.Data;
+import lombok.*;
 
 @Data
 @Builder
+@AllArgsConstructor
+@RequiredArgsConstructor
 public class Page<T> {
     private Page<T> previous;
     private Page<T> next;
+    @NonNull
     private T[] content;
 }
